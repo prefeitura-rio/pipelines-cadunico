74bced81ef65f6c0813c8647051fb16e78b754de
Gabriel Gazola Milan <gabriel.gazola@poli.ufrj.br>
2023-08-18 15:33:27 -0300

    fix: add missing import

--- pipelines/flows.py
+++ pipelines/flows.py
@@ -3,5 +3,6 @@
 Imports all flows for every project so we can register all of them.
 """
 # pylint: disable=W0401,W0614,W0611
+from pipelines.cadunico import *  # noqa
 from pipelines.exemplo import *  # noqa
 from pipelines.utils import *  # noqa
