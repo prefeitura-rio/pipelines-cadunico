c86798d92eb2b55805525b94c6ebe58cf0f42f7a
Gabriel Gazola Milan <gabriel.gazola@poli.ufrj.br>
2023-08-21 15:10:31 -0300

    feat: add template flow for running dbt models

--- pipelines/flows.py
+++ pipelines/flows.py
@@ -5,3 +5,4 @@ Imports all flows for every project so we can register all of them.
 # pylint: disable=W0401,W0614,W0611
 from pipelines.cadunico import *  # noqa
 from pipelines.exemplo import *  # noqa
+from pipelines.templates import *  # noqa
---
+++ pipelines/templates/run_dbt_model/flows.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+from prefect import Parameter
+from prefect.run_configs import KubernetesRun
+from prefect.storage import GCS
+
+from pipelines.constants import constants
+from pipelines.custom import CustomFlow as Flow
+from pipelines.templates.run_dbt_model.tasks import run_dbt_model_task
+
+with Flow(
+    name="Template: Executa modelo do dbt",
+) as templates__run_dbt_model__flow:
+    # Parameters
+    dataset_id = Parameter("dataset_id")
+    table_id = Parameter("table_id", default=None, required=False)
+    dbt_alias = Parameter("dbt_alias", default=False, required=False)
+    upstream = Parameter("upstream", default=None, required=False)
+    downstream = Parameter("downstream", default=None, required=False)
+    exclude = Parameter("exclude", default=None, required=False)
+    flags = Parameter("flags", default=None, required=False)
+    vars = Parameter("vars", default=None, required=False)
+
+    run_dbt_model_task(
+        dataset_id=dataset_id,
+        table_id=table_id,
+        dbt_alias=dbt_alias,
+        upstream=upstream,
+        downstream=downstream,
+        exclude=exclude,
+        flags=flags,
+        vars=vars,
+    )
+
+# Storage and run configs
+templates__run_dbt_model__flow.storage = GCS(constants.GCS_FLOWS_BUCKET.value)
+templates__run_dbt_model__flow.run_config = KubernetesRun(image=constants.DOCKER_IMAGE.value)
