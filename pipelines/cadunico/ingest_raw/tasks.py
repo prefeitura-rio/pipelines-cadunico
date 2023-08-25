78e46c874185e787f9836bd86ea54b78779130f2
d116626 <d116626@gmail.com>
2023-08-25 15:27:31 -0300

    chore: add models

--- pipelines/cadunico/ingest_raw/tasks.py
+++ pipelines/cadunico/ingest_raw/tasks.py
@@ -295,7 +295,7 @@ def get_tables_to_materialize(dataset_id: str, ingested_files_output: str | Path
                     if key == "versao_layout_particao":
                         versions.append(value)
     versions = list(set(versions))
-    log(f"FOUND Versions: {versions}")
+    log(f"FOUND VERSIONS: {versions}")
 
     root_path = get_root_path()
     queries_dir = root_path / f"queries/models/{dataset_id}"
@@ -313,6 +313,6 @@ def get_tables_to_materialize(dataset_id: str, ingested_files_output: str | Path
                 "dbt_alias": dbt_alias,
             }
             if version in table_id:
-                log(f"Append table to materialize: {dataset_id}.{table_id}")
+                log(f"Append table to materialize: {version} - {dataset_id}.{table_id}")
                 parameters_list.append(parameters)
     return parameters_list
