cb53ab602dbb92ceec9f5c742579cfbdf7e598c6
Gabriel Gazola Milan <gabriel.gazola@poli.ufrj.br>
2023-08-28 10:50:35 -0300

    fix: dbt models

--- queries/models/protecao_social_cadunico_versao/98_0609.sql
+++ queries/models/protecao_social_cadunico_versao/98_0609.sql
@@ -21,7 +21,7 @@ SELECT
     SUBSTRING(text,48,70) AS nom_prefeitura_pre,
     SAFE_CAST(versao_layout_particao AS STRING) AS versao_layout_particao,
     SAFE_CAST(data_particao AS DATE) AS data_particao
-FROM `rj-smas.protecao_solical_cadunico_staging.registro_familia`
+FROM `rj-smas.protecao_social_cadunico_staging.registro_familia`
 WHERE SAFE_CAST(data_particao AS DATE) < CURRENT_DATE('America/Sao_Paulo')
     AND versao_layout_particao = '0609'
     AND SUBSTRING(text,38,2) = '98'
