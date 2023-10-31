424b8cdd3a184923b7077f4b9d3d7512f5c34365
d116626 <d116626@gmail.com>
2023-10-31 17:44:04 -0300

    feat: add infra queries

--- queries/models/protecao_socal_cadunico_dashboard/numero_familia_quilombola_indigena.sql
+++ queries/models/protecao_socal_cadunico_dashboard/numero_familia_quilombola_indigena.sql
@@ -5,7 +5,7 @@ SELECT
         ELSE id_cras_creas
     END AS id_cras_creas,
   COUNT(DISTINCT id_familia) AS numero_familias,
-    SUM(CASE WHEN id_familia_indigena = '1' THEN 1 ELSE 0 END) AS numero_familias_indigenas,
+    SUM(CASE WHEN familia_indigena = '1' THEN 1 ELSE 0 END) AS numero_familias_indigenas,
     SUM(CASE WHEN familia_quilombola = '1' THEN 1 ELSE 0 END) AS numero_familias_quilombolas
 FROM `rj-smas.protecao_social_cadunico.familia`
 GROUP BY 1, 2
